832ac20c10776ae9fb23246b65f811fb2dc0758f
Oliver Foster <oliverfoster27@gmail.com>
2022-01-01 16:25:38 -0500

    s2_a2

---
+++ MyWork/s2_a2/js/main.js
@@ -0,0 +1,30 @@
+/*
+*    main.js
+*    Mastering Data Visualization with D3.js
+*    2.7 - Loading external data
+*/
+
+d3.json("/data/buildings.json").then(data => {
+
+	data.forEach(d => {
+		d.height = Number(d.height)
+	})
+
+	const svg = d3.select("#chart-area").append("svg")
+		.attr("width", 500)
+		.attr("height", 500)
+
+	const rectangles = svg.selectAll("rect")
+		.data(data)
+
+	rectangles.enter().append("rect")
+		.attr("x", (d, i) => (i * 50) + 50)
+		.attr("y", 50)
+		.attr("width", 30)
+		.attr("height", (d, i) => d.height)
+		.attr('stroke', 'black')
+		.attr('fill', '#69a3b2')
+
+}).catch(error => {
+	console.log(error)
+})
